330f2ecfc65c1756b88fa158094983ca91128f8d
mikilukasik <szarhazi@gmail.com>
2016-01-30 04:45:37 +0000

    .

--- demo1 - Copy/test1.js
+++
@@ -1,5 +0,0 @@
-if(mix("A aaaa bb c","& aaa bbb c d")=="1:aaaa/2:bbb"){
-    console.log('mix("A aaaa bb c","& aaa bbb c d")=="1:aaaa/2:bbb"')
-}else{
-    throw new Error('mix("A aaaa bb c","& aaa bbb c d")!="1:aaaa/2:bbb"')
-}
